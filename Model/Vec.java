3d37d0268bb0d521d549011cc0f0e49311519250
Pierrick <pierrick.graf@utbm.fr>
2018-06-25 20:59:31 +0200

    Javadoc Vec

--- Model/Vec.java
+++ Model/Vec.java
@@ -76,19 +76,35 @@ public class Vec {
         y *= ord;
     }
 
-    
+    /** Multiplication operation between two vectors.
+     *
+     * @param v the vector to be multiplied to the calling vector.
+     */
     void mult(Vec v) {
         x *= v.x;
         y *= v.y;
     }
+
+    /** Computes the norm of the vector.
+     *
+     * @return the norm of the vector.
+     */
     double mag() {
         return sqrt(pow(x, 2) + pow(y, 2));
     }
 
+    /** Computes the dot product between two vectors.
+     *
+     * @param v the vector used for the dot product.
+     * @return the dot product between the calling vector and the parameter vector.
+     */
     double dot(Vec v) {
         return x * v.x + y * v.y;
     }
 
+    /** Normalizes the calling vector, transforming it in a unit vector.
+     *
+     */
     void normalize() {
         double mag = mag();
         if (mag != 0) {
@@ -97,6 +113,12 @@ public class Vec {
         }
     }
 
+    /** Limits the vector length (norm) to the defined limit norm.
+     * This either changes nothing to the vector coordinates or makes them
+     * smaller to match the required limit.
+     *
+     * @param lim the limit norm.
+     */
     void limit(double lim) {
         double mag = mag();
         if (mag != 0 && mag > lim) {
@@ -105,23 +127,52 @@ public class Vec {
         }
     }
 
+    /** Computes the polar coordinates from the rectangular coordinates of the
+     * vector and returns theta.
+     *
+     * @return return theta from polar coordinates (r, theta).
+     */
     double heading() {
         return atan2(y, x);
     }
 
-    static Vec sub(Vec v, Vec v2) {
-        return new Vec(v.x - v2.x, v.y - v2.y);
+    /** Subtract two given vectors and return the result of the operation.
+     *
+     * @param v1 first vector of the operation.
+     * @param v2 second vector of the operation.
+     * @return return the vector result of the subtraction v1 - v2.
+     */
+    static Vec sub(Vec v1, Vec v2) {
+        return new Vec(v1.x - v2.x, v1.y - v2.y);
     }
 
-    static double dist(Vec v, Vec v2) {
-        return sqrt(pow(v.x - v2.x, 2) + pow(v.y - v2.y, 2));
+    /** Computes the distance between two vectors.
+     *
+     * @param v1 first vector of the operation.
+     * @param v2 second vector of the operation.
+     * @return return the distance between the two vectors as a double.
+     */
+    static double dist(Vec v1, Vec v2) {
+        return sqrt(pow(v1.x - v2.x, 2) + pow(v1.y - v2.y, 2));
     }
+
+    /** Computes the distance between the calling vector and the parameter vector.
+     *
+     * @param v the vector to compare for the distance.
+     * @return return the distance between the two vectors as a double.
+     */
     double dist(Vec v) {
         return sqrt(pow(x - v.x, 2) + pow(y - v.y, 2));
     }
 
-    static double angleBetween(Vec v, Vec v2) {
-        return acos(v.dot(v2) / (v.mag() * v2.mag()));
+    /** Computes the angle between two given vectors.
+     *
+     * @param v1 the first vector of the operation.
+     * @param v2 the second vector of the operation.
+     * @return the angle between both vectors as double.
+     */
+    static double angleBetween(Vec v1, Vec v2) {
+        return acos(v1.dot(v2) / (v1.mag() * v2.mag()));
     }
 
     public void setX(double x) {
@@ -133,11 +184,11 @@ public class Vec {
     }
 
     public double getX(){
-        return this.x;
+        return x;
     }
 
     public double getY(){
-        return this.y;
+        return y;
     }
 
 
