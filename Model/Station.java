83eee2c3d9c6fa1bbd8bcdd10464f8d513b118f4
Pierrick <pierrick.graf@utbm.fr>
2018-06-25 22:13:16 +0200

    Javadoc Station

--- Model/Station.java
+++ Model/Station.java
@@ -1,59 +1,58 @@
 package Model;
 
-import java.util.Random;
-
-/**
+/** Implementation of the station where the drones can dock and drop packages.
  *
+ * @author Pierrick GRAF
+ * @author Raphaël GRONDIN
  */
 public class Station extends Element{
 	private int droneCapacity;
 	private int packageCapacity;
 
-	/**
+	/** Constructor of the Station which creates an Element of type Station.
 	 *
-	 * @param droneCapacity
-	 * @param packageCapacity
-	 * @param x
-	 * @param y
+	 * @param droneCapacity capacity of drones the station can contain simultaneously.
+	 * @param packageCapacity capacity of packages the station can contain simultaneously.
+	 * @param x X coordinates of the station.
+	 * @param y Y coordinates of the station.
 	 */
 	public Station(int droneCapacity, int packageCapacity, int x, int y) {
 		super();
 		this.setSize(100);
-		Random rnd = new Random();
 		this.setX(x+getSize()/2);
 		this.setY(y+getSize()/2);
 		this.droneCapacity = droneCapacity;
 		this.packageCapacity = packageCapacity;
-		this.setImage("src/Model/_img/drone/helicopter.png");
+		this.setImage("src/Model/_img/drone/helicopter.png"); //$NON-NLS-1$
 	}
 
-	/**
+	/** Getter of the droneCapacity variable.
 	 *
-	 * @return
+	 * @return return the amount of drones that the Station can contain simultaneously.
 	 */
 	public int getDroneCapacity() {
 		return this.droneCapacity;
 	}
 
-	/**
+	/** Setter of the droneCapacity variable.
 	 *
-	 * @param droneCapacity
+	 * @param droneCapacity the amount of drones that the Station will be available to contain.
 	 */
 	public void setDroneCapacity(int droneCapacity) {
 		this.droneCapacity = droneCapacity;
 	}
 
-	/**
+	/** Getter of the packageCapacity variable.
 	 *
-	 * @return
+	 * @return return the amount of packages that the Station can contain simultaneously.
 	 */
 	public int getPackageCapacity() {
 		return this.packageCapacity;
 	}
 
-	/**
+	/** Setter of the packageCapacity variable.
 	 *
-	 * @param packageCapacity
+	 * @param packageCapacity the amount of packages that the Station will be available to contain.
 	 */
 	public void setPackageCapacity(int packageCapacity) {
 		this.packageCapacity = packageCapacity;
